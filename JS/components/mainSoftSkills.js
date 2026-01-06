f9a9a62c2c07dbd4c5233016bc995d04317a1fb2
YesMyTomaTTich <moumengamer18@gmail.com>
2026-01-06 16:00:30 +0100

    finally got it presentable for tomorrow

--- JS/components/mainSoftSkills.js
+++ JS/components/mainSoftSkills.js
@@ -55,12 +55,12 @@ export function mainSoftSkills() {
                     </div>
                 </div>
             </div>
-            <div class="softSkillsContainer">
-                <div class="skill1">
-                    <div class="circularProgress1 bar1">
+            <div class="softSkillsContainer softSkillsContainer2">
+                <div class="skill4">
+                    <div class="circularProgress4 bar4">
                         <div class="outer">
                             <div class="inner">
-                                <div class="number n1">
+                                <div class="number n4">
                                 </div>
                             </div>
                         </div>
@@ -68,13 +68,13 @@ export function mainSoftSkills() {
                         <circle cx="80" cy="80" r="70" stroke-linecap="round" />
                         </svg>
                     </div>
-                    <p>TeamWork <span>!</span></p>
+                    <p>Adaptability <span>!</span></p>
                 </div>
-                <div class="skill2">
-                    <div class="circularProgress2 bar2">
+                <div class="skill5">
+                    <div class="circularProgress5 bar5">
                         <div class="outer">
                             <div class="inner">
-                                <div class="number n2">
+                                <div class="number n5">
                                 </div>
                             </div>
                         </div>
@@ -82,13 +82,13 @@ export function mainSoftSkills() {
                         <circle cx="80" cy="80" r="70" stroke-linecap="round" />
                         </svg>
                     </div>
-                    <p>Communication <span>!</span></p>
+                    <p>Time Management <span>!</span></p>
                 </div>
-                <div class="skill3">
-                    <div class="circularProgress3 bar3">
+                <div class="skill6">
+                    <div class="circularProgress6 bar6">
                         <div class="outer">
                             <div class="inner">
-                                <div class="number n3">
+                                <div class="number n6">
                                 </div>
                             </div>
                         </div>
@@ -96,7 +96,7 @@ export function mainSoftSkills() {
                         <circle cx="80" cy="80" r="70" stroke-linecap="round" />
                         </svg>
                     </div>
-                    <p>Problem Solving <span>!</span></p>
+                    <p>Decision Making <span>!</span></p>
                 </div>
             </div>
         </div>
@@ -104,12 +104,15 @@ export function mainSoftSkills() {
     const number1 = document.querySelector(".n1");
     const number2 = document.querySelector(".n2");
     const number3 = document.querySelector(".n3");
+    const number4 = document.querySelector(".n4");
+    const number5 = document.querySelector(".n5");
+    const number6 = document.querySelector(".n6");
     function calcNumber() {
         let i = 0;
         const interval = setInterval(() => {
             i++;
             number1.textContent = i + "%";
-            if (i === 100) {
+            if (i === 99) {
                 clearInterval(interval);
             }
         }, 30);
@@ -119,7 +122,7 @@ export function mainSoftSkills() {
         const interval2 = setInterval(() => {
             i++;
             number2.textContent = i + "%";
-            if (i === 67) {
+            if (i === 90) {
                 clearInterval(interval2);
             }
         }, 30);
@@ -129,12 +132,45 @@ export function mainSoftSkills() {
         const interval3 = setInterval(() => {
             i++;
             number3.textContent = i + "%";
-            if (i === 50) {
+            if (i === 85) {
                 clearInterval(interval3);
+            }
+        }, 30);
+    }
+    function calcNumber4() {
+        let i = 0;
+        const interval4 = setInterval(() => {
+            i++;
+            number4.textContent = i + "%";
+            if (i === 75) {
+                clearInterval(interval4);
+            }
+        }, 30);
+    }
+    function calcNumber5() {
+        let i = 0;
+        const interval5 = setInterval(() => {
+            i++;
+            number5.textContent = i + "%";
+            if (i === 80) {
+                clearInterval(interval5);
+            }
+        }, 30);
+    }
+    function calcNumber6() {
+        let i = 0;
+        const interval6 = setInterval(() => {
+            i++;
+            number6.textContent = i + "%";
+            if (i === 80) {
+                clearInterval(interval6);
             }
         }, 30);
     }
     calcNumber();
     calcNumber2();
     calcNumber3();
+    calcNumber4();
+    calcNumber5();
+    calcNumber6();
 }
